8fc93b9d5e4c5a79c080001cdc89f20e32aed1f9
Giovanni Sakti <giosakti@gmail.com>
2017-12-06 19:21:13 +0700

    fix formatting using

--- repl/repl.go
+++ repl/repl.go
@@ -3,30 +3,30 @@
 package repl
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "github.com/giosakti/go-monkey/lexer"
-  "github.com/giosakti/go-monkey/token"
+	"bufio"
+	"fmt"
+	"github.com/giosakti/go-monkey/lexer"
+	"github.com/giosakti/go-monkey/token"
+	"io"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-  scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
-  for {
-    fmt.Printf(PROMPT)
-    scanned := scanner.Scan()
-    if !scanned {
-      return
-    }
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
 
-    line := scanner.Text()
-    l := lexer.New(line)
+		line := scanner.Text()
+		l := lexer.New(line)
 
-    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-      fmt.Printf("%+v\n", tok)
-    }
-  }
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
 }
